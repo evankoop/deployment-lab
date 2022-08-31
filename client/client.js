4162a7d4e1e5b67b02854cc1f31c87b426d857a8
Evan <koopevan@gmail.com>
2022-08-31 14:51:02 -0600

    Added functionality to button

---
+++ client/client.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('button')
+
+const buyPlantBtn = () => {
+    alert('Thank you so so so so so so so so much for buying this plant')
+}
+
+btn.addEventListener('click', buyPlantBtn)
